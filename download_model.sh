edc765b8af23598db13c2c59de4745a6a3aef54d
Katherine Crowson <crowsonkb@gmail.com>
2016-09-10 05:25:24 -0700

    Display link to the VGG model page on download

--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Downloads the 19-layer VGG ILSVRC-2014 model (http://www.robots.ox.ac.uk/~vgg/research/very_deep/).
+echo 'Downloading the 19-layer VGG ILSVRC-2014 model (http://www.robots.ox.ac.uk/~vgg/research/very_deep/).'
 
 curl -O 'http://www.robots.ox.ac.uk/%7Evgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel'
