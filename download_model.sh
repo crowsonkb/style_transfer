95b33b43e321234e7c6b927580fcab97005bd66f
Katherine Crowson <crowsonkb@gmail.com>
2016-10-12 00:06:55 -0700

    Move VGG model download path to own S3 bucket

--- download_model.sh
+++ download_model.sh
@@ -2,4 +2,4 @@
 
 echo 'Downloading the 19-layer VGG ILSVRC-2014 model (http://www.robots.ox.ac.uk/~vgg/research/very_deep/).'
 
-curl -O 'http://www.robots.ox.ac.uk/%7Evgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel'
+curl -O 'https://style-transfer.s3-us-west-2.amazonaws.com/VGG_ILSVRC_19_layers.caffemodel'
