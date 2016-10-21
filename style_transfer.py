d07d1c9a9cc4d1acef9cc5cc973192befb15cd32
Katherine Crowson <crowsonkb@gmail.com>
2016-10-21 14:45:46 -0700

    Pylint

--- style_transfer.py
+++ style_transfer.py
@@ -217,8 +217,8 @@ class AdamOptimizer:
 
 class LBFGSOptimizer:
     """Implements the L-BFGS quasi-Newton optimizer [6] with polynomial-decay averaging [2]."""
-    def __init__(self, params, step_size=1, averaging=True, avg_decay=1, n_corr=10, c1=1.02, c2=0.9,
-                 max_ls_fevals=10):
+    def __init__(self, params, step_size=1, averaging=True, avg_decay=1, n_corr=10, c1=1.02,
+                 c2=0.9, max_ls_fevals=10):
         """Initializes the optimizer."""
         self.params = params
         self.step_size = step_size
